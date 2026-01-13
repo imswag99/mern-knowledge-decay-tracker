dd25ee54652a2072418dd6f141179929f7c468f1
Swagat Talukdar <swagattalukdar007@gmail.com>
2026-01-14 01:42:40 +0530

    Updated frontend url in backend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-knowledge-decay-tracker.vercel.app",
     credentials: true
 }));
 
