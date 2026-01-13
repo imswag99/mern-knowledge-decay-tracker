cc490940cc37a874c92482e2bd72434ce9cbf058
Swagat Talukdar <swagattalukdar007@gmail.com>
2026-01-14 01:54:55 +0530

    Updated backend url in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://mern-knowledge-decay-tracker.onrender.com/api",
   withCredentials: true, // cookies enabled
 });
 
