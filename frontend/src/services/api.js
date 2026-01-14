306c468b4f6a60e9587b89001596d27119aa1f0f
Swagat Talukdar <swagattalukdar007@gmail.com>
2026-01-14 11:20:19 +0530

    Updated vercel.json to check if application works for mobile

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://mern-knowledge-decay-tracker.onrender.com/api",
   withCredentials: true, // cookies enabled
 });
 
